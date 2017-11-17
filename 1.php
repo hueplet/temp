2087216274f5328720ba4979bb94ed7656d56db5
tgmed <tagankamed@bk.ru>
2017-11-17 17:24:51 +0300

    first commit

---
+++ 1.php
@@ -0,0 +1,51 @@
+<?php
+declare(strict_types=1);
+
+echo "Hello, World!\n";
+
+
+/**
+* Books
+*/
+class Books
+{
+	public $title = "Title";
+	public $price = 0;
+
+	function __construct(string $title, int $price)
+	{
+		$this->title = $title;
+		$this->price = $price;
+	}
+}
+
+
+
+/**
+* 
+*/
+class BookWiter
+{
+	
+	function __construct()
+	{
+		# code...
+	}
+
+
+	public function write(Books $book) {
+		echo "Title: ".$book->title."\n";
+		echo "Price: ".$book->price."\n";
+	}
+}
+
+
+class Wrong{ }
+
+$book1 = new Books("Наполеон", 100);
+
+$writer = new BookWiter();
+$writer->write($book1);
+
+
+//var_dump($book1);
